a17cca3ab2aa4e4926e7bd85eb67fc3897ea6d2e
ptafo <ptafo@LAPTOP-5ODT2AD5>
2017-03-03 13:49:59 -0500

    changes

--- Chess42/src/chess/Chess.java
+++ Chess42/src/chess/Chess.java
@@ -2,4 +2,48 @@ package chess;
 
 public class Chess {
 
+		public static void main(String[]args){
+			
+			//this is just a test
+			ChessPiece[][] board= new ChessPiece[8][8];
+			for(int i= 0; i< board.length ;i++ ){
+				for(int j= 0; j< board[i].length; j++){
+					board[i][j]= null;
+				}
+			}
+			
+			
+			board[7][4] = new King("black");
+			boolean emp = true;
+			int count = 8;
+			for(int i= 0; i< board.length ;i++ ){
+				for(int j= 0; j< board[i].length; j++){
+					if(board[i][j]==null){
+						if(emp){
+							System.out.print("  ");
+							emp = false;
+						}else{
+							System.out.print(" ##");
+							emp = true;
+						}
+					}else{
+					board[i][j].getName();
+						if(emp){
+							emp = false;
+						}else{
+							emp = true;
+						}
+					}
+				}
+				if(emp){
+					emp = false;
+				}else{
+					emp = true;
+				}
+				System.out.print(" "+count--);
+				System.out.println();
+			}
+		}
+		
+		
 }
